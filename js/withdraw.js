5a6646f4e58e8bd265322080488591d8ebf1bde8
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-04-03 10:50:57 +0600

    event

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,4 +1,14 @@
+/*
+1. add withdraw button event handler.
+2. get withdraw amount by using getInputFieldValueById function.
+3. get previous withdraw amount by using getTextElementValueById function.
+4. calculate a new withdraw total and set the value.
+4.5 . set new withdraw total by using setTextElementValueById function.
+*/
+
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount = getInputFieldValueById('withdraw-field');
-    
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById('withdraw-total', newWithdrawTotal)
 })
